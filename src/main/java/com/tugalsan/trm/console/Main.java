263238bed019ee26dc0c96833f757a2ed31ce202
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-01-28 14:16:48 +0300

    Update Main.java

--- src/main/java/com/tugalsan/trm/console/Main.java
+++ src/main/java/com/tugalsan/trm/console/Main.java
@@ -3,12 +3,11 @@ package com.tugalsan.trm.console;
 import com.tugalsan.api.console.server.TS_ConsoleUtils;
 import com.tugalsan.api.file.pom.server.TS_FilePomConfig;
 import com.tugalsan.api.list.client.TGS_ListUtils;
-import com.tugalsan.api.log.server.TS_Log;
 import java.util.List;
 
 public class Main {
 
-    final private static TS_Log d = TS_Log.of(true, Main.class);
+//    final private static TS_Log d = TS_Log.of(true, Main.class);
     final public static TS_FilePomConfig pomCfg = TS_FilePomConfig.of();
 
     //cd C:\me\codes\com.tugalsan\trm\com.tugalsan.trm.trm
